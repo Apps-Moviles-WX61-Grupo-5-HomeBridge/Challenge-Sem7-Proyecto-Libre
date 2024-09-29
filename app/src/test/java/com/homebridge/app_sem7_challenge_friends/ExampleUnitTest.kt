ac0dd9e913b7133cf0e2c8976bb55ab701b9a66a
Camila Amaro <camilaamarov@yahoo.es>
2024-09-29 11:10:47 -0500

    feat: Add new project

---
+++ app/src/test/java/com/homebridge/app_sem7_challenge_friends/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.homebridge.app_sem7_challenge_friends
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
